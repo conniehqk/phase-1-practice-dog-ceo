0276136869841f5c4f703d32cfaa34397bbb454a
conniekang <hqk1121@gmail.com>
2021-09-28 19:23:24 -0400

    commit tree de6710d772fcebf9a4ae22b20c1ce8036666b932

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 console.log('%c HI', 'color: firebrick')
+
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+function renderImg(images) {
+    images.message.forEach(function(imglink) {
+        let imgContainer = document.getElementById('dog-image-container')
+        let imgTag = document.createElement('img')
+        imgTag.src = imglink
+        imgContainer.appendChild(imgTag)
+    });
+}
+function ceo() {
+    fetch(imgUrl).then(resp=>resp.json()).then(json=>renderImg(json))
+}
+document.addEventListener('DOMContentLoaded', ceo)
+
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+function changeColor(element) {
+    element.style.color = 'blue'
+}
+
+function renderBreed(object) {
+    for (const name in object.message) {
+        let breedContainer = document.getElementById('dog-breeds')
+        let breed = document.createElement('li')
+        breed.id = name
+        breed.textContent = name
+        breed.addEventListener('click', function() {
+            document.getElementById(name).style.color = 'blue'
+        })
+        document.getElementById('breed-dropdown').addEventListener('change', function(event) {
+            const letter = event.target.value
+            if (name[0]===letter) {
+                breedContainer.appendChild(breed)
+            }
+        })
+        breedContainer.appendChild(breed)
+    }
+}
+function breed() {
+    fetch(breedUrl).then(resp=>resp.json()).then(json=>renderBreed(json))
+}
+document.addEventListener('DOMContentLoaded', breed)
